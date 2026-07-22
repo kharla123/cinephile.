32c33536c25af52e29e6445a4e64ebf4ea0a0246
unknown <kharla123>
2026-07-22 08:58:10 +0200

    lista

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -5,7 +5,7 @@ export default function ExplorePage() {
     <main className="p-6">
       <h1 className="text-3xl font-semibold text-[#ede9e1] mb-4">Explorar películas</h1>
       <p className="text-[#8e8799] text-base">
-        Bienvenido a la página de exploración. Aquí podrás buscar y descubrir tus próximas películas favoritas.
+        Bienvenidos a la página de exploración. Aquí podrás buscar y descubrir tus próximas películas favoritas para que disfruten.
       </p>
     </main>
   )
